560113b0008c15a07e76ec09ccc46ef9f2be109b
jcbarros24 <126578116+jcbarros24@users.noreply.github.com>
2024-10-13 12:09:43 -0300

    Primeiro commit

---
+++ src/app/index/index.tsx
@@ -0,0 +1,26 @@
+import { Image, View, TouchableOpacity } from "react-native"
+import { MaterialIcons } from "@expo/vector-icons";
+
+import { styles } from "./sytles";
+import { colors } from "@/styles/colors";
+
+import { Category } from "@/components/category";
+
+
+export default function Index(){
+    return(
+        <View style={styles.container}>
+            <View style={styles.header}>
+                <Image source={require("@/assets/logo.png")} style={styles.logo} />
+                
+                <TouchableOpacity activeOpacity={0.6}>
+                    <MaterialIcons name="add" size={32} color={colors.green[300]} />
+                </TouchableOpacity>
+            </View>
+
+            <Category name="Projeto" icon="code" isSelected/>
+            <Category name="Site" icon="language" isSelected={false}/>
+            <Category name="Video" icon="movie" isSelected={false}/>
+        </View>
+    )
+}
